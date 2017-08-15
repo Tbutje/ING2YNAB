07271c0213de9f0952e8bced38a8d18241b9c218
Tbutje <timo.koole@gmail.com>
2017-08-15 21:17:04 +0200

    cleaned up code

--- ING2YNAB/RowReader.py
+++ ING2YNAB/RowReader.py
@@ -2,17 +2,6 @@
 class RowReader(object):
 
     ms_row = []
-    coDiverseBoekingen      = 'DV'
-    
-#   not checked yet    
-    coCrediteurenBetaling   = 'cb'
-    coGeldAutomaat          = 'ga'
-    coBetaalAutomaat        = 'ba'
-    coBetaalContactLoos     = 'bc'
-    coBankGiro              = 'bg'
-    coEuroIncasso           = 'ei'
-    coIdeal                 = 'id'
-    coEigenRek              = 'tb'
 
     def __init__(self, row):
         self.ms_row = row
@@ -23,27 +12,9 @@ class RowReader(object):
         lv_day = self.ms_row[0][6:8]
         return lv_day + '/' + lv_month + '/' + lv_year
     def get_payee(self):
-        lv_pay_type = self.ms_row[4]
         
-        if ( lv_pay_type == self.coGeldAutomaat
-        or   lv_pay_type == self.coBetaalAutomaat
-        or   lv_pay_type == self.coBetaalContactLoos):
-            return self.ms_row[10]  
-        
-        elif (  lv_pay_type == self.coBankGiro
-             or lv_pay_type == self.coCrediteurenBetaling
-             or lv_pay_type == self.coEuroIncasso
-             or lv_pay_type == self.coIdeal
-             or lv_pay_type == self.coEigenRek):
-            return self.ms_row[5] + " - " + self.ms_row[6]
-        
-        elif lv_pay_type == self.coDiverseBoekingen:
-            return self.ms_row[1]
+        return self.ms_row[1]
 
-        else:
-            return self.ms_row[5]
-          
-   
     def get_category(self): 
         return ''
     
